f80173d84ba0571835432e8e062443752a06ca72
Demian Godon <demiangodon@Demians-MacBook-Air.local>
2024-03-14 20:13:34 -0700

    config file updates

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'agxbnk',
   e2e: {
     baseUrl: "http://localhost:3001",
     setupNodeEvents(on, config) {
