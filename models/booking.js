25a38626d7c0052b6f49357548dae2885ddab5d2
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2026-03-27 12:21:33 +0000

    Dropped nedb in favour of lokijs

--- models/booking.js
+++ models/booking.js
@@ -1,65 +1,72 @@
-const Datastore = require('nedb');
+const loki = require('lokijs');
+
 let counter = 0;
+const db = new loki('booking.db');
+const booking = db.addCollection('bookings');
 
-const booking = new Datastore();
+exports.getIDs = function(query, callback) {
+  try {
+    // Convert nedb-style query to LokiJS query
+    const results = booking.find(query);
+    callback(null, results);
+  } catch (err) {
+    callback(err);
+  }
+};
 
-exports.getIDs = function(query, callback){
-  booking.find(query, function(err, booking){
-    if(err){
-      callback(err);
-    } else {
-      callback(null, booking);
+exports.get = function(id, callback) {
+  try {
+    const result = booking.findOne({ bookingid: parseInt(id) });
+    callback(null, result);
+  } catch (err) {
+    callback(err, null);
+  }
+};
+
+exports.create = function(payload, callback) {
+  try {
+    counter++;
+    payload.bookingid = counter;
+    booking.insert(payload);
+    callback(null, payload);
+  } catch (err) {
+    callback(err);
+  }
+};
+
+exports.update = function(id, updatedBooking, callback) {
+  try {
+    const doc = booking.findOne({ bookingid: parseInt(id) });
+    if (!doc) {
+      return callback(new Error(`Booking ${id} not found`));
     }
-  });
-},
+    Object.assign(doc, updatedBooking);
+    booking.update(doc);
+    callback(null);
+  } catch (err) {
+    callback(err);
+  }
+};
 
-exports.get = function(id, callback){
-  booking.findOne({'bookingid': parseInt(id)}, function(err, booking) {
-    if(err){
-      callback(err, null)
-    } else {
-      callback(null, booking);
+exports.delete = function(id, callback) {
+  try {
+    const doc = booking.findOne({ bookingid: parseInt(id) });
+    if (!doc) {
+      return callback(new Error(`Booking ${id} not found`));
     }
-  });
-},
+    booking.remove(doc);
+    callback(null);
+  } catch (err) {
+    callback(err);
+  }
+};
 
-exports.create = function(payload, callback){
-  counter++;
-  payload.bookingid = counter;
-
-  booking.insert(payload, function(err, doc) {
-    if(err){
-      callback(err);
-    } else {
-      callback(null, payload);
-    }
-  });
-},
-
-exports.update = function(id, updatedBooking, callback){
-  booking.update({'bookingid': parseInt(id)}, { $set: updatedBooking }, {}, function(err){
-    if(err){
-      callback(err);
-    } else {
-      callback(null);
-    }
-  });
-},
-
-exports.delete = function(id, callback){
-  booking.remove({'bookingid': parseInt(id)}, function(err){
-    if(err){
-      callback(err);
-    } else {
-      callback(null);
-    }
-  })
-},
-
-exports.deleteAll = function(callback){
-  counter = 0;
-
-  booking.remove({}, function(err){
-    callback();
-  });
-}
+exports.deleteAll = function(callback) {
+  try {
+    counter = 0;
+    booking.clear();
+    callback(null);
+  } catch (err) {
+    callback(err);
+  }
+};
